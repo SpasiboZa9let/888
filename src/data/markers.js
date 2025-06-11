bf6036a13793142b35ba2b19572d326e6c9d4155
Иван <foxneversleep@gmail.com>
2025-06-11 10:20:16 +0300

    Update markers.js

--- src/data/markers.js
+++ src/data/markers.js
@@ -1,5 +1,20 @@
 export const MARKERS = [
-  { x: 0.2, y: 0.6, img: 'assets/photos/1.jpg', text: 'Воспоминание у маяка на закате' },
-  { x: 0.5, y: 0.3, img: 'assets/photos/2.jpg', text: 'Прогулка по лесной тропе' },
-  { x: 0.8, y: 0.7, img: 'assets/photos/3.jpg', text: 'Пикник в парке' }
+  {
+    x: 0.2,
+    y: 0.6,
+    img: './assets/photos/1.jpg',
+    text: 'Воспоминание у маяка на закате'
+  },
+  {
+    x: 0.5,
+    y: 0.3,
+    img: './assets/photos/2.jpg',
+    text: 'Прогулка по лесной тропе'
+  },
+  {
+    x: 0.8,
+    y: 0.7,
+    img: './assets/photos/3.jpg',
+    text: 'Пикник в парке'
+  }
 ];
