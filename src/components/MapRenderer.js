bdb4672588a5595ef818af3c726beb0e01a3c23d
Иван <foxneversleep@gmail.com>
2025-06-11 10:09:49 +0300

    Update MapRenderer.js

--- src/components/MapRenderer.js
+++ src/components/MapRenderer.js
@@ -1,31 +1,30 @@
-// src/components/MapRenderer.js
-import { createEl, onResize } from '../utils/dom.js';
+import { qs } from '../utils/dom.js';
 
 export default class MapRenderer {
   constructor(mapSelector, markers, panel) {
-    this.map = document.querySelector(mapSelector);
+    this.mapEl   = qs(mapSelector);
     this.markers = markers;
-    this.panel = panel;
-    this._markerEls = [];
-    this._init();
+    this.panel   = panel;
+    this._bindResize();
+    this._renderMarkers();
   }
 
-  _init() {
+  _renderMarkers() {
+    // Очистить старые
+    this.mapEl.querySelectorAll('.marker').forEach(m => m.remove());
+    const { width, height } = this.mapEl.getBoundingClientRect();
+
     this.markers.forEach(data => {
-      const el = createEl('div', { className: 'marker' });
-      this.map.appendChild(el);
-      el.addEventListener('click', () => this.panel.show(data));
-      this._markerEls.push({ el, data });
-    });
-    this._updatePositions();
-    onResize(() => this._updatePositions());
-  }
-
-  _updatePositions() {
-    const { width, height } = this.map.getBoundingClientRect();
-    this._markerEls.forEach(({ el, data }) => {
+      const el = document.createElement('div');
+      el.className = 'marker';
       el.style.left = `${data.x * width}px`;
       el.style.top  = `${data.y * height}px`;
+      el.addEventListener('click', () => this.panel.show(data));
+      this.mapEl.appendChild(el);
     });
+  }
+
+  _bindResize() {
+    window.addEventListener('resize', () => this._renderMarkers());
   }
 }
