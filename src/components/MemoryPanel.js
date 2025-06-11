efa144a0d787fda7ffd97b95995d49e4dd5fdb9d
Иван <foxneversleep@gmail.com>
2025-06-11 10:43:24 +0300

    Update MemoryPanel.js

--- src/components/MemoryPanel.js
+++ src/components/MemoryPanel.js
@@ -1,30 +1,20 @@
-// src/components/MemoryPanel.js
 import { qs } from '../utils/dom.js';
 
 export default class MemoryPanel {
   constructor(selector) {
-    this.panelEl  = qs(selector);
-    this.imgEl    = qs('img', this.panelEl);
-    this.textEl   = qs('.text', this.panelEl);
-    this.overlay  = qs('#overlay');
-
-    // у нас больше не нужен крестик-клик, но можно всё оставить
-    const closeBtn = qs('.close', this.panelEl);
-    closeBtn?.addEventListener('click', () => this.hide());
-    this.overlay?.addEventListener('click', () => this.hide());
+    this.panel = qs(selector);
+    this.img   = qs('img', this.panel);
+    this.txt   = qs('.text', this.panel);
   }
 
-  show({ img, text }) {
-    this.imgEl.src   = img.startsWith('./') ? img : `./${img}`;
-    this.imgEl.alt   = text;
-    this.textEl.textContent = text;
-
-    this.overlay?.classList.add('visible');
-    this.panelEl.classList.add('visible');
+  show(data) {
+    this.img.src = data.img;
+    this.img.alt = data.text;
+    this.txt.textContent = data.text;
+    this.panel.classList.add('visible');
   }
 
   hide() {
-    this.overlay?.classList.remove('visible');
-    this.panelEl.classList.remove('visible');
+    this.panel.classList.remove('visible');
   }
 }
