2ffda1f34eac957cc29a89f2907cf83d79635ae4
Иван <foxneversleep@gmail.com>
2025-06-11 10:10:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { MARKERS } from './data/markers.js';
-import MapRenderer from './components/MapRenderer.js';
-import MemoryPanel from './components/MemoryPanel.js';
+import MapRenderer   from './components/MapRenderer.js';
+import MemoryPanel   from './components/MemoryPanel.js';
+import { startEmojiSmall } from './animations/emojiSmall.js';
+import { startEmojiLarge } from './animations/emojiLarge.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const mapEl = document.getElementById('map');
   const panel = new MemoryPanel('#memory-panel');
-  new MapRenderer(mapEl, MARKERS, panel).render();
+  new MapRenderer('#map', MARKERS, panel);
+  startEmojiSmall();
+  startEmojiLarge();
 });
