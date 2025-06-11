9781265ce2352d7d5cb4d4d1626dd5b69bb8af2a
Иван <foxneversleep@gmail.com>
2025-06-11 10:20:31 +0300

    Update MemoryPanel.js

--- src/components/MemoryPanel.js
+++ src/components/MemoryPanel.js
@@ -2,21 +2,28 @@ import { qs } from '../utils/dom.js';
 
 export default class MemoryPanel {
   constructor(selector) {
-    this.panelEl = qs(selector);
-    this.imgEl   = qs('img', this.panelEl);
-    this.textEl  = qs('.text', this.panelEl);
+    this.panelEl  = qs(selector);
+    this.imgEl    = qs('img', this.panelEl);
+    this.textEl   = qs('.text', this.panelEl);
     this.closeBtn = qs('.close', this.panelEl);
+    this.overlay  = document.getElementById('overlay');
+
     this.closeBtn.addEventListener('click', () => this.hide());
+    this.overlay.addEventListener('click', () => this.hide());
   }
 
   show({ img, text }) {
-    this.imgEl.src = img;
+    // гарантируем корректный путь
+    this.imgEl.src = img.startsWith('./') ? img : `./${img}`;
     this.imgEl.alt = text;
     this.textEl.textContent = text;
+
+    this.overlay.classList.add('visible');
     this.panelEl.classList.add('visible');
   }
 
   hide() {
+    this.overlay.classList.remove('visible');
     this.panelEl.classList.remove('visible');
   }
 }
