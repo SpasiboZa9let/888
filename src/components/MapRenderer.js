673c95f9183b44d2bc311c8a55a4673f8d29ec13
Иван <foxneversleep@gmail.com>
2025-06-11 12:45:12 +0300

    Update MapRenderer.js

--- src/components/MapRenderer.js
+++ src/components/MapRenderer.js
@@ -12,16 +12,12 @@ export default class MapRenderer {
     this.markers = markers;
     this.panel   = panel;
 
-    // Всегда привязываемся к ресайзу, чтобы маркеры правильно позиционировались
-    this._onResize = this._renderMarkers.bind(this);
-    window.addEventListener('resize', this._onResize);
-
-    // Первая отрисовка
+    window.addEventListener('resize', () => this._renderMarkers());
     this._renderMarkers();
   }
 
   _renderMarkers() {
-    // Удаляем старые
+    // Удаляем старые маркеры
     this.mapEl.querySelectorAll('.marker').forEach(el => el.remove());
 
     const { width, height } = this.mapEl.getBoundingClientRect();
@@ -29,19 +25,15 @@ export default class MapRenderer {
     this.markers.forEach(data => {
       const el = document.createElement('div');
       el.className = 'marker';
+      // позиционируем
       el.style.left = `${data.x * width}px`;
       el.style.top  = `${data.y * height}px`;
 
-      // Появление/скрытие панели по наведению
+      // показываем/скрываем панель по hover
       el.addEventListener('mouseenter', () => this.panel.show(data));
       el.addEventListener('mouseleave', () => this.panel.hide());
 
       this.mapEl.appendChild(el);
     });
-  }
-
-  // Если нужно, можно отключить ресайз-слушатель
-  destroy() {
-    window.removeEventListener('resize', this._onResize);
   }
 }
