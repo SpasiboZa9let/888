d4a3715220b90fade568c714badb86e63b150228
Иван <foxneversleep@gmail.com>
2025-06-11 10:09:20 +0300

    Update dom.js

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,14 +1,6 @@
-// src/utils/dom.js
-export function createEl(tag, props = {}) {
-  const el = document.createElement(tag);
-  Object.assign(el, props);
-  return el;
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
 }
-
-export function onResize(fn) {
-  window.addEventListener('resize', fn);
-}
-
-export function randomBetween(min, max) {
-  return min + Math.random() * (max - min);
+export function qsa(selector, parent = document) {
+  return Array.from(parent.querySelectorAll(selector));
 }
