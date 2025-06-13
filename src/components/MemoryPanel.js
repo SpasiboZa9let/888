ced5c450ebf84bb1ff469b8ba4c9d00a4e263079
Иван <foxneversleep@gmail.com>
2025-06-13 22:49:12 +0300

    Update MemoryPanel.js

--- src/components/MemoryPanel.js
+++ src/components/MemoryPanel.js
@@ -5,16 +5,35 @@ export default class MemoryPanel {
     this.panel = qs(selector);
     this.img   = qs('img', this.panel);
     this.txt   = qs('.text', this.panel);
+
+    this._isMobile = window.innerWidth <= 768; // порог можно уточнить
   }
 
   show(data) {
-    this.img.src = data.img;
-    this.img.alt = data.text;
-    this.txt.textContent = data.text;
-    this.panel.classList.add('visible');
+    if (!this.panel) return;
+
+    // на мобилках сначала прячем, потом показываем
+    if (this._isMobile) {
+      this.panel.classList.remove('visible');
+
+      setTimeout(() => {
+        this.img.src = data.img;
+        this.img.alt = data.text;
+        this.txt.textContent = data.text;
+        this.panel.classList.add('visible');
+      }, 300); // 300мс = как в transition
+    } else {
+      // десктоп: мгновенное обновление
+      this.img.src = data.img;
+      this.img.alt = data.text;
+      this.txt.textContent = data.text;
+      this.panel.classList.add('visible');
+    }
   }
 
   hide() {
-    this.panel.classList.remove('visible');
+    if (this.panel) {
+      this.panel.classList.remove('visible');
+    }
   }
 }
