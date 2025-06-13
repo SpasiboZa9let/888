fc80a624fe5b190790225202e839d0b361552c85
Иван <foxneversleep@gmail.com>
2025-06-13 23:27:48 +0300

    Update markers.js

--- src/data/markers.js
+++ src/data/markers.js
@@ -1,12 +1,12 @@
 export const MARKERS = [
-  { x: 0.50, y: 0.12, img: 'photos/1.jpg', text: 'Завтрак в саду' },
-  { x: 0.62, y: 0.18, img: 'photos/2.jpg', text: 'Прогулка по мосту' },
-  { x: 0.75, y: 0.32, img: 'photos/3.jpg', text: 'Рыночная площадь' },
-  { x: 0.78, y: 0.50, img: 'photos/4.jpg', text: 'Кафе у стены' },
-  { x: 0.65, y: 0.68, img: 'photos/5.jpg', text: 'Дворик с пальмами' },
-  { x: 0.50, y: 0.75, img: 'photos/6.jpg', text: 'Сквер с фонтаном' },
-  { x: 0.35, y: 0.68, img: 'photos/7.jpg', text: 'Балкон с видом' },
-  { x: 0.22, y: 0.50, img: 'photos/8.jpg', text: 'Улица с апельсинами' },
-  { x: 0.25, y: 0.30, img: 'photos/9.jpg', text: 'Арка с тенью' },
-  { x: 0.50, y: 0.50, img: 'photos/10.jpg', text: 'Закат на крыше' }
+  { x: 0.50, y: 0.12, img: 'assets/photos/1.jpg', text: 'Завтрак в саду' },
+  { x: 0.62, y: 0.18, img: 'assets/photos/2.jpg', text: 'Прогулка по мосту' },
+  { x: 0.75, y: 0.32, img: 'assets/photos/3.jpg', text: 'Рыночная площадь' },
+  { x: 0.78, y: 0.50, img: 'assets/photos/4.jpg', text: 'Кафе у стены' },
+  { x: 0.65, y: 0.68, img: 'assets/photos/5.jpg', text: 'Дворик с пальмами' },
+  { x: 0.50, y: 0.75, img: 'assets/photos/6.jpg', text: 'Сквер с фонтаном' },
+  { x: 0.35, y: 0.68, img: 'assets/photos/7.jpg', text: 'Балкон с видом' },
+  { x: 0.22, y: 0.50, img: 'assets/photos/8.jpg', text: 'Улица с апельсинами' },
+  { x: 0.25, y: 0.30, img: 'passets/hotos/9.jpg', text: 'Арка с тенью' },
+  { x: 0.50, y: 0.50, img: 'assets/photos/10.jpg', text: 'Закат на крыше' }
 ];
