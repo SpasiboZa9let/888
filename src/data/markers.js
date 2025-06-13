1c19bd14ca6ad7d3aacb3ad6bfe41c4d7581932e
Иван <foxneversleep@gmail.com>
2025-06-13 12:20:39 +0300

    Update markers.js

--- src/data/markers.js
+++ src/data/markers.js
@@ -1,8 +1,7 @@
-// Координаты и контент маркеров
 export const MARKERS = [
-  { x: 0.15, y: 0.25, img: './assets/photos/1.jpg', text: 'Воспоминание у маяка' },
-  { x: 0.5,  y: 0.35, img: './assets/photos/2.jpg', text: 'Прогулка по лесу' },
-  { x: 0.8,  y: 0.7,  img: './assets/photos/3.jpg', text: 'Пикник в парке' },
-  { x: 0.3,  y: 0.6,  img: './assets/photos/1.jpg', text: 'Вечер у костра' },
-  { x: 0.65, y: 0.2,  img: './assets/photos/2.jpg', text: 'Утренняя роса' }
+  { x: 0.1, y: 0.2, img: "assets/photos/1.jpg", text: "Воспоминание 1" },
+  { x: 0.25, y: 0.35, img: "assets/photos/2.jpg", text: "Воспоминание 2" },
+  { x: 0.42, y: 0.32, img: "assets/photos/3.jpg", text: "Воспоминание 3" },
+  { x: 0.58, y: 0.4, img: "assets/photos/4.jpg", text: "Воспоминание 4" },
+  { x: 0.7, y: 0.55, img: "assets/photos/5.jpg", text: "Воспоминание 5" },
 ];
