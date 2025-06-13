f7a0ea6926d3a318375ffb6fd4e8c35280e05bf1
Иван <foxneversleep@gmail.com>
2025-06-13 23:43:43 +0300

    Update MemoryPanel.js

--- src/components/MemoryPanel.js
+++ src/components/MemoryPanel.js
@@ -6,23 +6,42 @@ export default class MemoryPanel {
     this.img   = qs('img', this.panel);
     this.txt   = qs('.text', this.panel);
 
-    // создаём элемент для титров
     this.titleEl = document.createElement('div');
     this.titleEl.className = 'memory-title';
     this.panel.appendChild(this.titleEl);
+
+    this.isMobile = window.innerWidth < 768;
+    this.queue = Promise.resolve();
   }
 
   show(data) {
+    if (this.isMobile) {
+      this.queue = this.queue
+        .then(() => this._fadeOut())
+        .then(() => this._showData(data));
+    } else {
+      this._showData(data);
+    }
+  }
+
+  _showData(data) {
     this.img.src = data.img;
     this.img.alt = data.caption || '';
     this.txt.textContent = data.caption || '';
     this.titleEl.textContent = data.title || '';
     this.panel.classList.add('visible');
 
-    // запуск анимации
+    // перезапуск анимации титров
     this.titleEl.classList.remove('fade');
-    void this.titleEl.offsetWidth; // триггер перерисовки
+    void this.titleEl.offsetWidth;
     this.titleEl.classList.add('fade');
+  }
+
+  _fadeOut() {
+    return new Promise(resolve => {
+      this.panel.classList.remove('visible');
+      setTimeout(resolve, 250); // подстроено под CSS transition
+    });
   }
 
   hide() {
