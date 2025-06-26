7969e2be982d116c2b612c4da71574f857ecc86e
Иван <foxneversleep@gmail.com>
2025-06-26 11:30:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,6 +37,18 @@ function injectPinThumbnails() {
     pin.style.backgroundImage = `url(${imgPath})`;
   });
 }
+function wireClickSound() {
+  /* пины на карте */
+  document.querySelectorAll('#map .marker')
+    .forEach(pin => pin.addEventListener('click', () => audio.playClick()));
+
+  /* стрелки в альбоме (если нужны) */
+  ['.prev', '.next'].forEach(sel => {
+    const btn = document.querySelector(sel);
+    btn?.addEventListener('click', () => audio.playClick());
+  });
+}
+wireClickSound();
 
 
 
