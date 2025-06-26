aa2aa39bbbfe4be9edc87fecad31bf6b9e0490a7
Иван <foxneversleep@gmail.com>
2025-06-26 16:43:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig(({ mode }) => ({
-  // Cordova и локальная разработка → относительные пути
-  // GitHub Pages: перед сборкой вручную меняете на '/888/' (или свой slug)
+ 
   base: './',
 
   build: {
