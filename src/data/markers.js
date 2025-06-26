f41f2e34926d7f6e5e9bc9b8131e27f61daa6dc4
Иван <foxneversleep@gmail.com>
2025-06-26 13:20:15 +0300

    Update markers.js

--- src/data/markers.js
+++ src/data/markers.js
@@ -1,14 +1,14 @@
-import { photo } from '../utils/assetPath.js';   // добавляет ./photos/ prefix
+import { photo } from '../utils/assetPath.js';
 
 export const MARKERS = [
-  { x: 0.50,  y: 0.325, img: photo('1.jpg'),  title: 'Свет сквозь занавески'},
-  { x: 0.659, y: 0.396, img: photo('2.jpg'),  title: 'Завтрак в полутоне'},
-  { x: 0.780, y: 0.545, img: photo('3.jpg'),  title: 'На балконе'},
-  { x: 0.780, y: 0.705, img: photo('4.jpg'),  title: 'Случайный снимок'},
-  { x: 0.659, y: 0.854, img: photo('5.jpg'),  title: 'Жаркое небо'},
-  { x: 0.50,  y: 0.925, img: photo('6.jpg'),  title: 'Тени под деревом'},
-  { x: 0.341, y: 0.854, img: photo('7.jpg'),  title: 'Поцелуй за киоском'},
-  { x: 0.220, y: 0.705, img: photo('8.jpg'),  title: 'Трамвайный звон'},
-  { x: 0.220, y: 0.545, img: photo('9.jpg'),  title: 'Закат в переулке'},
-  { x: 0.341, y: 0.396, img: photo('10.jpg'), title: 'Ночная тишина'}
+  { x: 0.50,  y: 0.325, img: photo('1.jpg'),  title: 'Свет сквозь занавески' },
+  { x: 0.659, y: 0.396, img: photo('2.jpg'),  title: 'Завтрак в полутоне' },
+  { x: 0.780, y: 0.545, img: photo('3.jpg'),  title: 'На балконе' },
+  { x: 0.780, y: 0.705, img: photo('4.jpg'),  title: 'Случайный снимок' },
+  { x: 0.659, y: 0.854, img: photo('5.jpg'),  title: 'Жаркое небо' },
+  { x: 0.50,  y: 0.925, img: photo('6.jpg'),  title: 'Тени под деревом' },
+  { x: 0.341, y: 0.854, img: photo('7.jpg'),  title: 'Поцелуй за киоском' },
+  { x: 0.220, y: 0.705, img: photo('8.jpg'),  title: 'Трамвайный звон' },
+  { x: 0.220, y: 0.545, img: photo('9.jpg'),  title: 'Закат в переулке' },
+  { x: 0.341, y: 0.396, img: photo('10.jpg'), title: 'Ночная тишина' }
 ];
